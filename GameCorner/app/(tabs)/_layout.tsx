09971cd1e2e7fe695efd4e776289d2bf22cdfb61
Foucher Mathias <mathias.foucher@etu.univ-amu.fr>
2025-03-27 09:54:29 +0100

    tets

--- GameCorner/app/(tabs)/_layout.tsx
+++ GameCorner/app/(tabs)/_layout.tsx
@@ -34,9 +34,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="recherche"
         options={{
-          title: 'Explore',
+          title: 'Recherche',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
