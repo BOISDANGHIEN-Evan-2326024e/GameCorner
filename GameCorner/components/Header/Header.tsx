df986699b40573b2e5b188799eae02c29947e0f5
Foucher Mathias <mathias.foucher@etu.univ-amu.fr>
2025-04-01 09:54:20 +0200

    gros changements !

---
+++ GameCorner/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { TouchableOpacity, View } from "react-native";
+import { styles } from "./Header.styles";
+import React from "react";
+import { ThemedText } from "../ThemedText";
+import { Ionicons } from "@expo/vector-icons";
+
+
+export default function Header({ setPage }: { setPage: (page: string) => void }) {
+    return(
+
+    <View style={styles.header}>
+        <ThemedText style={{ color: '#FFFFFF', fontSize: 22, fontWeight: 'bold' }}>
+        GameCorner
+        </ThemedText>
+        <TouchableOpacity onPress={() => setPage("user")}>
+        <Ionicons name="person-circle-outline" size={28} color="#FFFFFF" />
+        </TouchableOpacity>
+    </View>
+    );
+}
